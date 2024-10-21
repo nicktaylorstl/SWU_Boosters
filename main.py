5e245fc527ac8d78c4e99b62e21ae32be3c95b43
Nick Taylor <nick.taylor.esl@gmail.com>
2024-10-21 13:39:31 +0200

    added for multiple packs

--- main.py
+++ main.py
@@ -25,10 +25,11 @@ for c in collection[1:]:
         full_card_pool.append(card)
 
 # This creates one booster pack according to the distribution rules
-def get_booster(set='any'):
+def get_booster(set='any',qty = "2",):
+
     card_pool = {}
-
-
+    packs = []
+    
     index = 0
     for c in full_card_pool:
         if (set == 'any' and c[0] != 'twi') or c[0] == set:
@@ -43,68 +44,80 @@ def get_booster(set='any'):
     rares = {}
     legendaries = {}
 
-    pack = []
-    for k, v in card_pool.items():
-        if v[4] == 'Leader': leaders[k] = v
-        elif v[4] == 'Base': bases[k] = v
-        elif v[17] == 'Common': commons[k] = v
-        elif v[17] == 'Uncommon': uncommons[k] = v
-        elif v[17] == 'Rare': rares[k] = v
-        elif v[17] == 'Legendary': legendaries[k] = v
+    for i in range(int(qty)):
 
-    # 1 Leader, 1 Base, 9 Commons, 3 Uncommons, 1 Rare/Legendary, 1 random card
-    leader_index = random.choice(list(leaders.keys()))
-    leader = copy.deepcopy(card_pool[leader_index])
-    leader.append('1 Leader')
-    pack.append(leader)
+        pack = []
+        for k, v in card_pool.items():
+            if v[4] == 'Leader': leaders[k] = v
+            elif v[4] == 'Base': bases[k] = v
+            elif v[17] == 'Common': commons[k] = v
+            elif v[17] == 'Uncommon': uncommons[k] = v
+            elif v[17] == 'Rare': rares[k] = v
+            elif v[17] == 'Legendary': legendaries[k] = v
 
-    base_index = random.choice(list(bases.keys()))
-    base = copy.deepcopy(card_pool[base_index])
-    base.append('1 Base')
-    pack.append(base)
+        # 1 Leader, 1 Base, 9 Commons, 3 Uncommons, 1 Rare/Legendary, 1 random card
+        leader_index = random.choice(list(leaders.keys()))
+        leader = copy.deepcopy(card_pool[leader_index])
+        leader.append('1 Leader')
+        pack.append(leader)
 
-    for i in range(9):
-        common_index = random.choice(list(commons.keys()))
-        common = copy.deepcopy(card_pool[common_index])
-        common.append(f"{i+1} of 9 Commons")
-        pack.append(common)
+        base_index = random.choice(list(bases.keys()))
+        base = copy.deepcopy(card_pool[base_index])
+        base.append('1 Base')
+        pack.append(base)
 
-    for i in range(3):
-        uncommon_index = random.choice(list(uncommons.keys()))
-        uncommon = copy.deepcopy(card_pool[uncommon_index])
-        uncommon.append(f"{i+1} of 3 Uncommons")
-        pack.append(uncommon)
+        for i in range(9):
+            common_index = random.choice(list(commons.keys()))
+            common = copy.deepcopy(card_pool[common_index])
+            common.append(f"{i+1} of 9 Commons")
+            pack.append(common)
 
-    random_number = random.randint(1, 8)
-    if len(legendaries) > 0 and random_number == 8:
-        rare_index = random.choice(list(legendaries.keys()))
-    else:
-        rare_index = random.choice(list(rares.keys()))
-    rare = copy.deepcopy(card_pool[rare_index])
-    rare.append("1 Rare or Legendary")
-    pack.append(rare)
+        for i in range(3):
+            uncommon_index = random.choice(list(uncommons.keys()))
+            uncommon = copy.deepcopy(card_pool[uncommon_index])
+            uncommon.append(f"{i+1} of 3 Uncommons")
+            pack.append(uncommon)
 
-    any_card_index = random.choice(list(card_pool.keys()))
-    any_card_type = card_pool[any_card_index][4]
-    while any_card_type == 'Leader' or any_card_type == 'Base':
+        random_number = random.randint(1, 8)
+        if len(legendaries) > 0 and random_number == 8:
+            rare_index = random.choice(list(legendaries.keys()))
+        else:
+            rare_index = random.choice(list(rares.keys()))
+        rare = copy.deepcopy(card_pool[rare_index])
+        rare.append("1 Rare or Legendary")
+        pack.append(rare)
+
         any_card_index = random.choice(list(card_pool.keys()))
         any_card_type = card_pool[any_card_index][4]
-    any_card = copy.deepcopy(card_pool[any_card_index])    
-    any_card.append("1 'Foil' of any rarity")
-    pack.append(any_card)
-
-    return pack
+        while any_card_type == 'Leader' or any_card_type == 'Base':
+            any_card_index = random.choice(list(card_pool.keys()))
+            any_card_type = card_pool[any_card_index][4]
+        any_card = copy.deepcopy(card_pool[any_card_index])    
+        any_card.append("1 'Foil' of any rarity")
+        pack.append(any_card)
+        packs.append(pack)
+    return packs
 
 @app.route('/')
 def index():
-    # Get the set from the query parameters, default to 'any'
-    swu_set = request.args.get('set', 'any')
+    # Get the set and quantity for each booster from query parameters
+    qty_sor = request.args.get('qty_sor', '0')
+    qty_shd = request.args.get('qty_shd', '0')
+    qty_twi = request.args.get('qty_twi', '0')
 
-    # Generate the booster pack based on the set
-    booster = get_booster(swu_set)
+    # Generate booster packs based on quantities selected by the user
+    boosters = []
 
-    # Render the HTML template with the booster pack and current set
-    return render_template('booster_pack.html', booster=booster, swu_set=swu_set)
+    if int(qty_sor) > 0:
+        boosters.extend(get_booster(set='sor', qty=qty_sor))
+    if int(qty_shd) > 0:
+
+        boosters.extend(get_booster(set='shd', qty=qty_shd))
+    if int(qty_twi) > 0:
+        boosters.extend(get_booster(set='twi', qty=qty_twi))
+
+    # Render the HTML template with the booster packs and current set
+    return render_template('booster_pack.html', boosters=boosters, swu_set='multiple')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
